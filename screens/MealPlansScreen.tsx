c336b80470bcf67dd955545c7d301004866547c1
KithminiOKD <112233369+KithminiOKD@users.noreply.github.com>
2025-08-20 23:43:49 +0530

    MealPlans

---
+++ screens/MealPlansScreen.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const MealPlansScreen = () => {
+  return (
+    <View>
+      <Text>MealPlansScreen</Text>
+    </View>
+  )
+}
+
+export default MealPlansScreen
+
+const styles = StyleSheet.create({})
