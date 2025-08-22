881958d0d7a7be27422f7a75c0aa50e7c0abea9b
KithminiOKD <112233369+KithminiOKD@users.noreply.github.com>
2025-08-22 23:34:42 +0530

    Update DishDetailScreen.tsx

--- screens/DishDetailScreen.tsx
+++ screens/DishDetailScreen.tsx
@@ -1,5 +1,14 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  Image,
+  TouchableOpacity,
+} from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { RouteProp, useRoute } from '@react-navigation/native';
+import axios from 'axios';
 
 type DishDetailParam = {
   DishDetail: {
@@ -18,13 +27,121 @@ interface RecipeDetail {
 }
 
 const DishDetailScreen = () => {
+  const route = useRoute<RouteProp<DishDetailParam, 'DishDetail'>>();
+  const { recipeId, title, image } = route.params;
+  const [recipe, setRecipe] = useState<RecipeDetail | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchRecipeDetails = async () => {
+      try {
+        const response = await axios.get(
+          `https://api.spoonacular.com/recipes/${recipeId}/information`,
+          {
+            params: {
+              apiKey: '3b743256e59f4613bc9580268e2a0b78',
+              includeNutrition: false,
+            },
+          },
+        );
+        setRecipe(response.data);
+      } catch (error) {
+        console.log('Error', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchRecipeDetails();
+  }, []);
+
   return (
-    <View>
-      <Text>DishDetailScreen</Text>
-    </View>
+    <ScrollView style={styles.container}>
+      <Image source={{ uri: image }} style={styles.image} />
+      <Text style={styles.title}>{title}</Text>
+
+      <View style={styles.section}>
+        <Text style={styles.sectionTitle}>Ingredients</Text>
+        {recipe?.extendedIngredients?.map((ingredient: any, index: number) => (
+          <Text style={styles.ingredient} key={index}>
+            {ingredient.original}
+          </Text>
+        ))}
+      </View>
+
+      <View style={styles.section}>
+        <Text style={styles.sectionTitle}>Steps</Text>
+        {recipe?.analyzedInstructions[0]?.steps.map(
+          (step: any, index: number) => (
+            <View style={styles.step}>
+              <Text style={styles.stepNumber}>Step {step.number}</Text>
+              <Text style={styles.stepText}>{step.step}</Text>
+            </View>
+          ),
+        )}
+      </View>
+      <TouchableOpacity style={styles.startButton}>
+        <Text style={styles.startButtonText}>Start Cooking</Text>
+      </TouchableOpacity>
+    </ScrollView>
   );
 };
 
 export default DishDetailScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  image: {
+    width: '100%',
+    height: 300,
+    resizeMode: 'cover',
+  },
+  title: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginVertical: 20,
+    color: '#333',
+  },
+  section: {
+    padding: 20,
+  },
+  sectionTitle: {
+    fontSize: 22,
+    fontWeight: '600',
+    color: '#ff2d55',
+    marginBottom: 10,
+  },
+  ingredient: {
+    fontSize: 16,
+    color: '#666',
+    marginBottom: 5,
+  },
+  step: {
+    marginBottom: 15,
+  },
+  stepNumber: {
+    fontSize: 15,
+    fontWeight: 'bold',
+    color: '#007AFF',
+  },
+  stepText: {
+    fontSize: 15,
+    color: '#666',
+  },
+  startButton: {
+    backgroundColor: '#ff2d55',
+    padding: 15,
+    borderRadius: 25,
+    margin: 20,
+    alignItems: 'center',
+  },
+  startButtonText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});
