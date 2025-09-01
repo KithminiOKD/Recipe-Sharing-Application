ebe8b927b46a077e215a4420c5596fed707e504d
KithminiOKD <112233369+KithminiOKD@users.noreply.github.com>
2025-09-01 17:39:52 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -5,31 +5,73 @@
  * @format
  */
 
-import { NavigationContainer } from '@react-navigation/native';
-import { StatusBar, StyleSheet, useColorScheme } from 'react-native';
+import React, { useEffect } from 'react';
+import type {PropsWithChildren} from 'react';
 import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
-import BottomTabNavigator from './navigation/AppNavigator';
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
 
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { NavigationContainer } from '@react-navigation/native';
+import BottomTabNavigator from './navigation/AppNavigator';
+import { Provider, useDispatch } from 'react-redux';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { jwtDecode } from 'jwt-decode';
+import { setUser, store } from './store';
+
+
+const AppContent = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const initializeAuth = async () => {
+      try{
+        const authData = await AsyncStorage.getItem("auth");
+        if(authData){
+          const {user,token} = JSON.parse(authData);
+          const decoded:any = jwtDecode(token);
+          const currentTime = Date.now()/ 1000;
+          if(decoded.exp < currentTime){
+            await AsyncStorage.removeItem("auth");
+            return;
+          }
+          dispatch(setUser({user,token}))
+        }
+      }catch(error){
+        console.log("Error",error);
+        await AsyncStorage.removeItem("auth")
+      }
+    }
+    initializeAuth()
+  },[dispatch]);
 
   return (
     <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
       <NavigationContainer>
         <BottomTabNavigator />
       </NavigationContainer>
     </SafeAreaProvider>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
+function App():React.JSX.Element{
+  return (
+    <Provider store={store}>
+      <AppContent/>
+    </Provider>
+  )
+}
 
 export default App;
