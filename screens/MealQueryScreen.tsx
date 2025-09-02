dd0df82a2f60b11bc1fc0ffb3f421c3d610b1843
KithminiOKD <112233369+KithminiOKD@users.noreply.github.com>
2025-09-02 21:19:06 +0530

    Create MealQueryScreen.tsx

---
+++ screens/MealQueryScreen.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const MealQueryScreen = () => {
+  return (
+    <View>
+      <Text>MealQueryScreen</Text>
+    </View>
+  )
+}
+
+export default MealQueryScreen
+
+const styles = StyleSheet.create({})
